f03e40fcad6e2cd86e9b0c1d37b05b91d67c859e
art200109 <66074399+art200109@users.noreply.github.com>
2020-05-31 18:41:56 +0300

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -7,5 +7,3 @@ docker run -dit --add-host docker_engine:$docker_engine_ip --name app app_image
 
 app_ip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' app`
 docker run -dit --add-host app:$app_ip --name proxy -p 80:80 proxy_image
-
-curl 127.0.0.1:80
