c8d7c265fe1285027b35ce18df61b08a4babf539
art200109 <66074399+art200109@users.noreply.github.com>
2020-05-31 04:56:11 +0300

    Update build_script.sh

--- nginx/build_script.sh
+++ nginx/build_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t proxy_image .
-#docker tag proxy_image art200109/proxy_image
-#docker push art200109/proxy_image
+docker tag proxy_image art200109/proxy_image
+docker push art200109/proxy_image
