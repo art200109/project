60f0cab0e53aae6293d06377aa0ef5f243827270
art200109 <66074399+art200109@users.noreply.github.com>
2020-05-31 07:12:31 +0300

    Update build_script.sh

--- flask/build_script.sh
+++ flask/build_script.sh
@@ -2,7 +2,7 @@
 mkdir ./.ssh/
 ssh-keygen -t rsa -C docker -f ./.ssh/docker_key -q -N "" <<< y
 
-cat ./docker_test/docker_key.pub >> /root/.ssh/authorized_keys
+cat ./.ssh/docker_key.pub >> /root/.ssh/authorized_keys
 
 docker build -t app_image \
 --build-arg ssh_prv_key="$(cat ./.ssh/docker_key)" \
