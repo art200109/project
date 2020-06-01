3f6477a444a3bc77d10567744b1cb01a649410c3
art200109 <66074399+art200109@users.noreply.github.com>
2020-06-01 21:31:08 +0300

    Add files via upload

---
+++ flask/flask_user/useradd.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+cp -n /bin/bash /bin/rbash
+
+mkdir -p ./flask_user/programs
+ln -s /usr/bin/docker ./flask_user/programs/
+
+cp ./.bash_profile ./flask_user
+
+mkdir ./flask_user/.ssh
+cp ./.ssh/docker_key.pub ./flask_user/.ssh/authorized_keys
+
+userdel -r flask_user > /dev/null
+useradd -s /bin/rbash flask_user
+cp -rf ./flask_user /home/
+chown -R flask_user /home/flask_user
+rm -rf ./flask_user
