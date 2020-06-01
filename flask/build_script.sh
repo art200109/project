fe56fb7f2be386a5fe49ab661d2b06e557f00f91
art200109 <66074399+art200109@users.noreply.github.com>
2020-06-01 22:45:19 +0300

    Update build_script.sh

--- flask/build_script.sh
+++ flask/build_script.sh
@@ -6,10 +6,11 @@ ssh-keygen -t rsa -C docker -f ./.ssh/docker_key -q -N "" <<< y
 
 chmod u+x ./useradd.sh
 ./useradd.sh
+cd ..
 
 docker build -t app_image \
---build-arg ssh_prv_key="$(cat ./.ssh/docker_key)" \
---build-arg ssh_pub_key="$(cat ./.ssh/docker_key.pub)" \
+--build-arg ssh_prv_key="$(cat ./flask_user/.ssh/docker_key)" \
+--build-arg ssh_pub_key="$(cat ./flask_user/.ssh/docker_key.pub)" \
 .
 
 docker tag app_image art200109/app_image
